463e26e9ad9c5a1a4db4b080c02ec198e42ad5e4
Arg0nn <lucasmagnar@gmail.com>
2019-05-15 23:37:37 -0300

    Adição da opção esqueci minha senha | Conecção com Banco de dados

--- app/Views/users/homepage.php
+++ app/Views/users/homepage.php
@@ -1,11 +1,38 @@
 <?php
 
-    $this->extend('layouts/layout_users');
+    $this->extend('layouts/layout_main');
     $s = session();
 
 ?>
 
 <?php $this->section('content')?>
+
+<!--/ Intro Single star /-->
+<section class="intro-single">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-lg-8">
+          <div class="title-single-box">
+            <h1 class="title-single">Login</h1>
+          </div>
+        </div>
+        <div class="col-md-12 col-lg-4">
+          <nav aria-label="breadcrumb" class="breadcrumb-box d-flex justify-content-lg-end">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <a href="<?php echo site_url('Main') ?>">Home</a>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                Login
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ Intro Single End /-->
+
 
     <div>Olá, <?php echo $s->name . ' (' . $s->id_user . ')'?></div>
 
--- app/Views/users/recover_password.php
+++ app/Views/users/recover_password.php
@@ -1,10 +1,40 @@
 <?php
 
-	$this->extend('layouts/layout_users');
+	$this->extend('layouts/layout_main');
 
 ?>
 
 <?php $this->section('content')?>
+
+<!--/ Intro Single star /-->
+<section class="intro-single">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-lg-8">
+          <div class="title-single-box">
+            <h1 class="title-single">Esqueci minha senha</h1>
+          </div>
+        </div>
+        <div class="col-md-12 col-lg-4">
+          <nav aria-label="breadcrumb" class="breadcrumb-box d-flex justify-content-lg-end">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <a href="<?php echo site_url('Main') ?>">Home</a>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+              <a href="<?php echo site_url('Users') ?>">Login</a>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                Esqueci minha senha
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ Intro Single End /-->
+
 
 <div class="container">
   <div class="row">
