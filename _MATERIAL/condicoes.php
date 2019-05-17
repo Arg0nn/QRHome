0295b9590fe1216f743a09c8bcbdd9b50e28ed95
samuka47 <50751256+samuka47@users.noreply.github.com>
2019-05-17 15:21:51 -0300

    Adicionado o ficheiro de condições

---
+++ _MATERIAL/condicoes.php
@@ -0,0 +1,23 @@
+<?php
+    defined('BASEPATH') OR exit('URL inválida.');
+?>
+
+O usuário deve ler atentamente os TERMOS DE CONDIÇÕES E USO antes de utilizar este website
+
+O QRHome atua como disponibilizador de anúncios vindos de diversas imobiliárias anunciantes e, portanto, não atua como prestador de serviços de consultoria ou ainda intermediário ou participante em nenhum negócio entre o USUÁRIO e o(s) ANUNCIANTE(S).
+
+Dessa forma, o SITE não assume responsabilidade por nenhuma conseqüência que possa advir de qualquer relação entre o USUÁRIO e o(s) ANUNCIANTE(S), seja ela direta ou indireta. Assim, o SITE não é responsável por qualquer ação ou omissão do USUÁRIO baseada nas informações, anúncios, fotos, vídeos ou outros materiais colocados no QRHome. Adicionalmente, o SITE envida seus melhores esforços para manter o QRHome sempre atualizado, preciso e completo, mas não se responsabiliza por imprecisão, erro, fraude, inexatidão ou divergência nos dados, fotos e vídeos ou outros materiais relacionados a anúncios ou à imprecisão das informações aqui contidas.
+
+Recomenda-se que na Internet toda transação seja realizada com cautela e bom senso redobrados. O USUÁRIO deverá avaliar os riscos das negociações, levando em consideração que pode estar, eventualmente, lidando com menores de idade ou pessoas valendo-se de falsas identidades.
+
+O SITE expressamente sugere que o USUÁRIO: (I) verifique cautelosamente toda a documentação e/ou características de qualquer dos produtos ou serviços ofertados antes da conclusão de qualquer negócio, concordando que ao negociar com o(s) ANUNCIANTE(S) dos produtos e serviços ofertados o faz por sua conta e risco; (II) verifique pessoalmente ou através de alguém de sua confiança qualquer bem ou serviço a ser adquirido; (IIIi) seja cuidadoso com os dados de sua identificação individual sempre que acessar a Internet, informando-os apenas em operações em que exista a proteção de dados; (IV) forneça informações verídicas e de sua titularidade; (V) seja responsável por qualquer tipo de informação ou afirmação originadas com seu nome de USUÁRIO e senha; (VI) mantenha em sigilo a sua identificação de USUÁRIO (nome e senha); (VII) desconecte sua conta do ZAP tão logo deixe de acessá-lo, principalmente se dividir o computador com outra pessoa; (VIII) cumpra rigorosamente todas as determinações deste Termo de Uso; (IX) tome quaisquer outras medidas necessárias para se proteger de danos, inclusive fraudes ou estelionato "on line".
+
+O SITE toma os cuidados necessários para evitar o envio não solicitado de e-mails. Por isso fica estabelecido que é absolutamente proibida a utilização da ferramenta: "Envie este link a um amigo" com a finalidade de Spam, ou envio indiscriminado de mensagens de qualquer natureza via correio eletrônico.
+
+O SITE poderá suspender, inabilitar definitivamente e aplicar as medidas jurídicas cabíveis aos USUÁRIOS que utilizarem esta ferramenta com a finalidade de promover seus produtos ou serviços à venda ou com qualquer outro fim, e que forem denunciados pelas pessoas que receberam essas mensagens.
+
+O QRHome oferece a possibilidade do anunciante (imobiliária) se cadastrar, e neste caso tal USUÁRIO pode ter acesso a informações, recursos ou facilidades exclusivas, responsabilizando-se o USUÁRIO civil e criminalmente pelas informações e dados fornecidos. O cadastro é gratuito e sujeito à análise. Não é permitido que um mesmo anunciante tenha mais de um cadastro. Se o QRHome detectar cadastros que aparentemente pertencem à mesma pessoa, poderá inabilitar definitivamente todos os cadastros.
+
+O QRHome se reserva o direito de recusar qualquer solicitação de cadastro e de cancelar um cadastro previamente aceito, a seu exclusivo critério e sem aviso prévio, não cabendo nenhuma indenização ou reparação pelo cancelamento de quaisquer cadastros ou de impossibilidade de cadastro.
+
+O SITE poderá modificar, alterar e/ou ajustar estes termos a qualquer tempo e essas modificações, alterações e/ou ajustes deverão ser efetivos e imediatos assim que estes se tornem públicos. Para este efeito, o USUÁRIO deverá rever os termos de uso periodicamente, sendo certo que o acesso ou uso contínuo do QRHome pelo USUÁRIO subordina-se à aceitação dos termos em vigor.
